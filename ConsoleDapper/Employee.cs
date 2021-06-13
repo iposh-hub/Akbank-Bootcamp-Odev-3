b49fde0f2dd2fae1f1f07c3a5348c03565754e58
iposh-hub <82669904+iposh-hub@users.noreply.github.com>
2021-06-13 12:30:34 +0300

    Add files via upload

---
+++ ConsoleDapper/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDapper
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string CellPhone { get; set; }
+
+
+        public void PrintEmployee()
+        {
+            string info = String.Format("Çalışan Id: {0}.\nÇalışanın Adı : {1}.\nÇalışanın Adresi: {2}.\nÇalışanın Cep Telefonu: {3}. ", Id, Name, Address, CellPhone);
+            Console.WriteLine(info);
+        }
+
+    }
+
+}
